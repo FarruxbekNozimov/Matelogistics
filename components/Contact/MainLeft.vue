49b4470254bdbf495be56486bd4adf43c35a700e
FarruxDEV <farruxbeknozimov@gmail.com>
2023-12-27 14:20:40 +0500

    fix bug

--- components/Contact/MainLeft.vue
+++ components/Contact/MainLeft.vue
@@ -1,6 +1,5 @@
-<script setup lang="ts">
-import type { FormError, FormSubmitEvent } from "#ui/types";
-
+<script setup>
+import { reactive } from "vue";
 const options = [
 	"I want to get a quote",
 	"I want to book a quote",
@@ -13,56 +12,60 @@ const state = reactive({
 	name: "",
 	email: "",
 	nbm: "",
-	subject: options[0],
+	subject: "",
 	message: "",
 });
 </script>
 
 <template>
 	<div class="space-y-4">
-		<UFormGroup label="Name" name="name">
+		<UFormGroup label="Name" name="name" required>
 			<UInput
 				color="blue"
 				variant="none"
-				v-model="state.name"
-				inputClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-white focus:border-blue-500"
+				:model-value="state.name"
+				inputClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-[#E8F0FF] focus:border-blue-500"
 				placeholder="Mike Thomas" />
 		</UFormGroup>
-		<UFormGroup label="Email" name="email">
+		<UFormGroup label="Email" name="email" required>
 			<UInput
 				color="blue"
 				variant="none"
-				v-model="state.email"
+				:model-value="state.email"
 				type="email"
-				inputClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-white focus:border-blue-500"
+				inputClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-[#E8F0FF] focus:border-blue-500"
 				placeholder="Your email" />
 		</UFormGroup>
-		<UFormGroup label="Phone" name="phone">
+		<UFormGroup label="Phone" name="phone" required>
 			<UInput
 				color="blue"
 				variant="none"
-				v-model="state.nbm"
+				:model-value="state.nbm"
 				type="number"
-				inputClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-white focus:border-blue-500"
+				inputClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-[#E8F0FF] focus:border-blue-500"
 				placeholder="Your phone number" />
 		</UFormGroup>
-		<UFormGroup label="Phone" name="phone">
+		<UFormGroup label="Subject" name="subject" required>
 			<USelectMenu
 				color="blue"
 				variant="none"
-				v-model="state.subject"
-				selectClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-white focus:border-blue-500"
+				:model-value="state.subject"
+				selectClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-[#E8F0FF] focus:border-blue-500"
 				:options="options" />
 		</UFormGroup>
-		<UTextarea
-			autoresize
-			selectClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-white focus:border-blue-500"
-			placeholder="Search..."
-			model-value="" />
+		<UFormGroup label="Message" name="message" required>
+			<UTextarea
+				color="blue"
+				variant="none"
+				autoresize
+				textareaClass="text-gray-500 p-2 px-3 rounded-xl text-[16px] font-[400] bg-[#E8F0FF] border border-[#E8F0FF] focus:border-blue-500"
+				placeholder="Search..."
+				:model-value="state.message" />
+		</UFormGroup>
 
 		<UButton
 			type="submit"
-			class="bg-[#008AFF] hover:bg-blue-500 rounded-xl p-3 px-7 text-[16px]">
+			class="bg-[#008AFF] hover:bg-blue-500 rounded-xl p-3 px-7 text-[16px] font-[600]">
 			Send message
 		</UButton>
 	</div>
