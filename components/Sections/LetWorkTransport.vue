371ae844c680e9f620b044902ad21d422a0aeb38
FarruxDEV <farruxbeknozimov@gmail.com>
2023-12-28 22:08:05 +0500

    let work transport section was fixed from bugs

--- components/Sections/LetWorkTransport.vue
+++ components/Sections/LetWorkTransport.vue
@@ -3,31 +3,37 @@
 <template>
 	<UContainer>
 		<div
-			class="bg-gradient-to-r from-[#008AFF] to-[#005BA8] p-7 rounded-2xl relative lg:flex items-center justify-between">
-			<div class="space-y-5 w-1/2">
-				<h3 class="text-white font-[700] text-[30px]">
+			class="bg-gradient-to-r from-[#008AFF] to-[#005BA8] md:p-7 p-3 rounded-2xl relative md:flex items-center justify-between">
+			<div class="space-y-5 md:w-1/2">
+				<h3 class="text-white font-[700] md:text-[30px]">
 					Let Us Work On Your Auto Transport
 				</h3>
 				<p class="text-white font-[400] text-[16px]">
 					Effortless Planning: Utilize Our Free and Live Cost Calculator for
 					Instant Transportation Estimates
 				</p>
-				<div class="flex items-center gap-5">
-					<button
-						class="w-full outline-none text-white bg-[#E52E2E] hover:bg-red-700 font-medium duration-300 rounded-2xl px-5 py-2.5 mb-2">
-						Use live cost calculator
-					</button>
-					<button
-						class="w-full outline-none text-[#008AFF] bg-white hover:bg-white/80 duration-300 font-medium rounded-2xl px-5 py-2.5 mb-2">
-						Call now (929) 592-3003
-					</button>
+				<div class="sm:flex items-center gap-5">
+					<img
+						src="@/assets/img/check-img.png"
+						alt="img"
+						class="md:hidden sm:h-[118px] sm:mb-0 mb-5" />
+					<div class="md:flex items-center gap-5">
+						<button
+							class="w-full whitespace-nowrap outline-none text-white bg-[#E52E2E] hover:bg-red-700 font-medium duration-300 rounded-2xl px-5 py-2.5 mb-2">
+							Use live cost calculator
+						</button>
+						<button
+							class="w-full whitespace-nowrap outline-none text-[#008AFF] bg-white hover:bg-white/80 duration-300 font-medium rounded-2xl px-5 py-2.5 mb-2">
+							Call now (929) 592-3003
+						</button>
+					</div>
 				</div>
 			</div>
 
 			<img
 				src="@/assets/img/check-img.png"
 				alt="img"
-				class="absolute right-24 bottom-0" />
+				class="absolute right-24 bottom-0 md:block hidden" />
 		</div>
 	</UContainer>
 </template>
