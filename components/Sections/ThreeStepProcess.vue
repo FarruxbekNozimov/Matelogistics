bdd1b9b86e99c3447fb7c55c3130525dc38b0bd6
mominovDev <mardonmominov6@gmail.com>
2024-01-30 17:52:25 +0500

    how aut transport is fixed

--- components/Sections/ThreeStepProcess.vue
+++ components/Sections/ThreeStepProcess.vue
@@ -1,29 +1,62 @@
+<script setup>
+defineProps(["img1", "img2", "img3"]);
+</script>
+
 <template>
   <div class="mx-auto min-w-[300px] max-w-[1200px] h-full">
     <TitleBar title="Effortless car shipping: A 3-step process" />
-    <div class="flex ">
+    <div class="flex mt-10 mb-12 justify-around">
       <div>
-        <h1 class="text-[#214690] lg:text-[24px] text-xl lg:px-4 font-bold">Book your quote</h1>
-        <p>Get a Free Quote: Book Your Car Shipping Now!</p>
-        <img src="@/assets/img/threeStep1.png" alt="user" class="" />
+        <div class="flex justify-center">
+          <h1 class="text-[#214690] lg:text-[24px] text-xl lg:px-4 font-bold">
+            Book your quote
+          </h1>
+        </div>
+        <div class="flex justify-center">
+          <p>Get a Free Quote:</p>
+        </div>
+        <div class="flex justify-center">
+          <p>Book Your Car Shipping Now!</p>
+        </div>
+        <div class="flex justify-center pt-3">
+          <img :src="`/img/${img1}`" alt="user" class="" />
+        </div>
       </div>
       <div>
-        <h1 class="text-[#214690] lg:text-[24px] text-xl lg:px-4 font-bold">Get your car picked up</h1>
-        <p>Convenient Car Pickup: Schedule Your Shipping Today!</p>
-        <img src="@/assets/img/threeStep2.png" alt="user" class="" />
+        <div class="flex justify-center">
+          <h1 class="text-[#214690] lg:text-[24px] text-xl lg:px-4 font-bold">
+            Get your car picked up
+          </h1>
+        </div>
+        <div class="flex justify-center">
+          <p>Convenient Car Pickup:</p>
+        </div>
+        <div class="flex justify-center">
+          <p>Schedule Your Shipping Today!</p>
+        </div>
+        <div class="flex justify-center pt-3">
+          <img :src="`/img/${img2}`" alt="user" class="" />
+        </div>
       </div>
       <div>
-        <h1 class="text-[#214690] lg:text-[24px] text-xl lg:px-4 font-bold">Enjoy your car</h1>
-        <p>Hit the Road: Enjoy Your Car After Seamless Delivery!</p>
-        <img src="@/assets/img/threeStep3.png" alt="user" class="" />
+        <div class="flex justify-center">
+          <h1 class="text-[#214690] lg:text-[24px] text-xl lg:px-4 font-bold">
+            Enjoy your car
+          </h1>
+        </div>
+        <div class="flex justify-center">
+          <p>Hit the Road:</p>
+        </div>
+        <div class="flex justify-center">
+          <p>Enjoy Your Car After Seamless Delivery!</p>
+        </div>
+        <div class="flex justify-center pt-3">
+          <img :src="`/img/${img3}`" alt="user" class="" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {};
-</script>
-
-<style lang="scss" scoped>
+<style scoped>
 </style>
--- pages/how-auto-transportation-works.vue
+++ pages/how-auto-transportation-works.vue
@@ -3,11 +3,39 @@
     <SectionsBanner
       title="How auto Transportation works"
       text="Discover the seamless 3-Step process of auto transportation! Mate logistics to move your car safely across distances using various transport modes. Experience reliable, efficient, and hassle-free auto transportation tailored to your needs."
-    />
+      noForm="1"/>
     <SectionsCustomerComments />
-    <SectionsThreeStepProcess/>
-    <SectionsReviews/>
-    <SectionsLetWorkTransport/>
+    <SectionsThreeStepProcess img1="threeStep1.png" img2="threeStep2.png" img3="threeStep3.png"/>
+    <SectionsTextImage
+      title="Get a Free Quote: Book Your Car Shipping Quote!"
+      text="Simply click or tap to receive an instant online quote within seconds, or reach out to our advisors at (929) 592-3003. Compare our prices and services — we're confident in our transparent pricing. Go ahead, explore, and discover the value we offer!
+Once you are ready to book with us, simply click the link in the email or while at cost calculator page or call us at (929) 592-3003 to schedule a carrier to pick up your vehicles
+We will contact you instantly for confirming the information and select one of our verified 10 000+ drivers to provide seamless auto transportation.
+The due now is $0, you will not pay a penny until your pick-up time is scheduled. And we provide full credit card payment at an extra option or cash option to pay — simply ask from our advisors on the call. 
+"
+      img="thresteep1.png"
+      firstImg="1"
+    />
+    <SectionsTextImage
+      title="Convenient Car Pickup: Schedule Your Shipping Today!"
+      text="Arrange the time, date, and a convenient pickup spot for our driver to pick up your vehicle. You can release the car while at home, at work, etc.
+The both of you inspect the vehicle and sign Bill Of Lading (BOL) before loading the car on the trailer. Then, you're free to go about your day while we handle the rest.
+Rest assured, your car will be in excellent hands, and you can stay updated via calling us at (929) 592-3003 or by directly contacting your assigned driver to track your vehicle's journey."
+      img="thresteep2.png"
+      isButton="1"
+    />
+    <SectionsTextImage
+      title="Hit the Road: 
+Enjoy Your Car After Seamless Delivery!"
+      text="Experience the Joy of Driving Post a Seamless Delivery!
+Your vehicle's journey ends with us, ensuring a seamless and smooth delivery process. Now, revel in the pleasure of being back behind the wheel as you enjoy the convenience and ease of our service. Drive away with confidence, knowing your car has been delivered to you hassle-free!
+Following a thorough check to ensure everything is in top shape (although we're confident it will be!), you'll just need to sign the Bill Of Lading to receive your delivery. And that's all there is to it!"
+      img="thresteep3.png"
+      firstImg="1"
+      isButton="1"
+    />
+    <SectionsReviews />
+    <SectionsLetWorkTransport />
   </div>
 </template>
 
